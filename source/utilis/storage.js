9b51c212fe53f2b56ac4327a2162486ed3f7b995
ben <benjamineruvieru@gmail.com>
2022-09-07 23:38:35 +0100

    feat: added mmkv storage

---
+++ source/utilis/storage.js
@@ -0,0 +1,17 @@
+import {MMKV} from 'react-native-mmkv';
+
+export const storage = new MMKV();
+
+export const setItem = (key, value, shouldStringify) => {
+  const mainvalue = shouldStringify ? JSON.stringify(value) : value;
+  storage.set(key, mainvalue);
+};
+
+export const getItem = (key, shouldParse) => {
+  const value = storage.getString(key);
+  return shouldParse ? JSON.parse(value) : value;
+};
+
+export const deleteItem = key => {
+  storage.delete(key);
+};
